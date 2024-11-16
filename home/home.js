720690ef3e8497e3927e5e1a2c9b4ac911a661c5
Riya Singh <riyasingh1731@gmail.com>
2024-11-16 18:05:21 +0530

    first commit

--- home/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.form-section form');
-  
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      alert('Form submitted successfully!');
-    });
-  });
-  
