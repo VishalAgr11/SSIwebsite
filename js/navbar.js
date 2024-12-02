ca97486712c9e0bf7adae4c41264e8d44fb577e7
Riya Singh <riyasingh1731@gmail.com>
2024-12-02 14:36:40 +0530

    transparent navbar added

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navbarLinks = document.querySelector('.navbar-links');
 
